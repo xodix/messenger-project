d5baba600fa06a44804d29b0b2bff67589d97402
klaus <kodemode@wp.pl>
2021-01-10 23:51:44 +0100

    connected chats to backend

--- client/src/pages/chats/Chats.tsx
+++ client/src/pages/chats/Chats.tsx
@@ -1,47 +1,69 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Nav from '../../components/Nav';
+import { UserContext } from './../../actions/mainContext'
+import { useHistory } from 'react-router-dom';
+
+import { Popup } from './../../components/Popup';
 
 function Chats() {
+  const history = useHistory();
+  const [groups, setGroups] = useState<{ name: string }[]>([{ name: '' }]);
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    if (!user.jwtToken) history.push('/login');
+    else {
+
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("x-auth-token", user.jwtToken);
+
+      const requestOptions: RequestInit = {
+        method: 'POST',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+
+      fetch("http://localhost:5000/g/get", requestOptions)
+        .then(response => response.json())
+        .then(result => setGroups(result), err => console.error(err));
+
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [display, setDisplay] = useState<'none' | 'block'>('none');
+
   return (
     <>
       <Nav />
+      <Popup hBodyName="groupCode" displayName="group code" display={display} setDisplay={setDisplay} />
       <main>
-        <div className="group">
-          <div className="group-logo"></div>
-          <div className="group-name">
-            <h1>Beaches crazy</h1>
-          </div>
-        </div>
-        <div className="group">
-          <div className="group-logo"></div>
-          <div className="group-name">
-            <h1>Beaches crazy</h1>
-          </div>
-        </div>
-        <div className="group">
-          <div className="group-logo"></div>
-          <div className="group-name">
-            <h1>Beaches crazy</h1>
-          </div>
-        </div>
-        <div className="group">
-          <div className="group-logo"></div>
-          <div className="group-name">
-            <h1>Beaches crazy</h1>
-          </div>
-        </div>
-        <div className="group">
-          <div className="group-logo"></div>
-          <div className="group-name">
-            <h1>Beaches crazy</h1>
-          </div>
-        </div>
-        <div className="group">
-          <div className="group-logo"></div>
-          <div className="group-name">
-            <h1>Beaches crazy</h1>
-          </div>
-        </div>
+        <button
+          style={{
+            borderRadius: '50%',
+            width: '2em',
+            fontSize: '1.5em',
+            margin: 'auto'
+          }}
+          onClick={() => setDisplay('block')}
+        >
+          +
+        </button>
+        {
+          !groups.length
+            ? <h1 style={{ textAlign: 'center' }}>You aren't involved in any groups!</h1>
+            : groups.map((group, i) => {
+              return (
+                <div className="group" key={i}>
+                  <div className="group-logo"></div>
+                  <div className="group-name">
+                    <h1>{group.name}</h1>
+                  </div>
+                </div>
+              )
+            })
+        }
       </main>
     </>
   )
