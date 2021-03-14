1f5f572c84b2df2c191891bdd6eb184fcd584f6c
klaus <kodemode@wp.pl>
2021-03-14 19:55:04 +0100

    added chat

--- server/app.ts
+++ server/app.ts
@@ -1,37 +1,34 @@
-import config from './config/';
-import mongoose from 'mongoose';
+import config from "./config/";
+import mongoose from "mongoose";
 
-mongoose.connect
-  (
-    config.MONGO_DB_URI as string,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      autoIndex: true,
-      useCreateIndex: true
-    }
-  )
+mongoose
+  .connect(config.MONGO_DB_URI as string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: true,
+    useCreateIndex: true,
+  })
   .then(() => console.log("Mongodb atlas conected!"));
-mongoose.connection.on('error', err => {
+mongoose.connection.on("error", (err) => {
   throw err;
 });
 
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-import friendRouter from './routes/friendshipsRouter';
-import groupRouter from './routes/groupRouter';
-import messageRouter from './routes/messageRouter';
-import userRouter from './routes/userRouter';
+import friendRouter from "./routes/friendshipsRouter";
+import groupRouter from "./routes/groupRouter";
+import messageRouter from "./routes/messageRouter";
+import userRouter from "./routes/userRouter";
 
-app.use('/f/', friendRouter);
-app.use('/g/', groupRouter);
-app.use('/m/', messageRouter);
-app.use('/u/', userRouter);
+app.use("/f/", friendRouter);
+app.use("/g/", groupRouter);
+app.use("/m/", messageRouter);
+app.use("/u/", userRouter);
 
 // conn check
 app.get("/", (req, res) => {
@@ -39,42 +36,48 @@ app.get("/", (req, res) => {
 });
 
 // 404s
-app.get('/*', (req, res) => {
+app.get("/*", (req, res) => {
   res.send(`can't GET ${req.url} 404 not found`).status(404);
 });
-app.post('/*', (req, res) => {
+app.post("/*", (req, res) => {
   res.send(`can't POST ${req.url} 404 not found`).status(404);
 });
-app.delete('/*', (req, res) => {
+app.delete("/*", (req, res) => {
   res.send(`can't DELETE ${req.url} 404 not found`).status(404);
 });
 
 // connecting to socket.io
-import http from 'http';
+import http from "http";
+import jwt from "jsonwebtoken";
 const server = http.createServer(app);
 // tslint:disable-next-line: no-var-requires
 const io = require("socket.io")(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', socket => {
-  console.log('user connected');
+io.on("connection", (socket) => {
+  console.log("user connected");
 
-  const { groupId } = socket.handshake.query;
+  const { groupId, jwtToken } = socket.handshake.query;
+
   socket.join(groupId);
 
-  socket.on("message", msg => {
-    console.log(msg);
-    io.to(groupId).broadcast.emit();
-    io.broadcast.emit("message", msg);
+  socket.on("message", async (msg: string) => {
+    console.log(`message sent: ${msg}}`);
+    try {
+      jwt.decode(jwtToken);
+      io.to(groupId).emit("message", { content: msg, nick: "anon" });
+    } catch (err) {
+      io.broadcast.emit("error", err);
+    }
   });
 
   socket.on("disconnect", () => {
     socket.leave(groupId);
-    console.log('user disconnected');
+    console.log("user disconnected");
   });
 });
 
